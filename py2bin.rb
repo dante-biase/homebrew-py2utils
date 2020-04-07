1ab089267273f744ef4dfbfff9ea11a5bf8992e2
Dante Biase <dante.biase@outlook.com>
2020-04-07 10:40:59 -0400

    fixed

--- py2bin.rb
+++ py2bin.rb
@@ -1,8 +1,8 @@
 class Py2bin < Formula
   desc ""
   homepage "https://github.com/dante-biase/py2bin"
-  url "(url omitted)"
-  sha256 "(sha from step 3)"
+  url "https://github.com/dante-biase/homebrew-py2utils/archive/v1.0.tar.gz"
+  sha256 "545418c7a748f70a68c6e3be211e132d4d70cf99c0292ee238e13100aa062831"
 
   bottle :unneeded
 

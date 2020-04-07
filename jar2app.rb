c6bd6fd15ddddd699a387046266f583268550d6a
Dante Biase <dante.biase@outlook.com>
2020-04-07 11:24:09 -0400

    fixed shasum

--- jar2app.rb
+++ jar2app.rb
@@ -2,7 +2,7 @@ class Jar2app < Formula
   desc "easily convert JAR files into a Mac OS Applications"
   homepage "https://github.com/dante-biase/jar2app"
   url "https://github.com/dante-biase/homebrew-x2appUtils/releases/download/v1.0/jar2app"
-  sha256 "350535c3e28e24aa14efdd3dcfca2c0b402da971394c44670065503be631c54d"
+  sha256 "f548ef452f3fb199ec02b399692104275cc61ef581c2570bf9029b1724c60bd2"
 
   bottle :unneeded
 

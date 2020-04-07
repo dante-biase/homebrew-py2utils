b63d97da801c8f7846ff3a1be4ca7a7ef74d2a6d
Dante Biase <dante.biase@outlook.com>
2020-04-07 10:55:47 -0400

    created py2bin formula

---
+++ py2bin.rb
@@ -0,0 +1,16 @@
+class Py2bin < Formula
+  desc "easily convert Python file(s) into a binaries"
+  homepage "https://github.com/dante-biase/py2bin"
+  url "https://github.com/dante-biase/homebrew-py2utils/releases/download/v1.0/py2bin"
+  sha256 "350535c3e28e24aa14efdd3dcfca2c0b402da971394c44670065503be631c54d"
+
+  bottle :unneeded
+
+  def install
+    bin.install "py2bin"
+  end
+
+  test do
+    system "#{bin}/py2bin", "--version"
+  end
+end

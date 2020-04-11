53274120bf9233221b0a7b7be8b9fb88fb34aa71
dante-biase <45440995+dante-biase@users.noreply.github.com>
2020-04-11 12:32:44 -0400

    Update py2app.rb

--- py2app.rb
+++ py2app.rb
@@ -2,7 +2,7 @@ class Py2app < Formula
   desc "easily convert Python file(s)/projects into Mac OS Applications"
   homepage "https://github.com/dante-biase/py2app"
   url "https://github.com/dante-biase/homebrew-x2x/releases/download/v1.0.1/py2app"
-  sha256 "97c6e37b04a5422bef61ac8002651eb8f40322f919006171d33822c067ad28f6"
+  sha256 "2008802a062a701f06010a8ba990eab57cec0f6cfae94d5a2b4675dc34c654c6"
 
   bottle :unneeded
 

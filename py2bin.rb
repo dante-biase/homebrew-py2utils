34ab3badf5390565ec47e6b9cc3fc9af7965da28
dante-biase <45440995+dante-biase@users.noreply.github.com>
2020-04-11 12:46:37 -0400

    Update py2bin.rb

--- py2bin.rb
+++ py2bin.rb
@@ -2,7 +2,7 @@ class Py2bin < Formula
   desc "easily convert Python file(s) into binaries"
   homepage "https://github.com/dante-biase/py2bin"
   url "https://github.com/dante-biase/homebrew-x2x/releases/download/v1.0.1/py2bin"
-  sha256 "86885021fb0d22e2f64f922eeafb1e87ace6524a18f7564347ba63927b0fd837"
+  sha256 "2b34a2fd84355e82653bb1ee6f1843b511fc84a5e6db0d1502b9c748a17bdffb"
 
   bottle :unneeded
 

be8fd21451a86c54737381b6990388128da7de2c
dante-biase <45440995+dante-biase@users.noreply.github.com>
2021-12-28 10:44:25 -0500

    Update jar2app.rb

--- jar2app.rb
+++ jar2app.rb
@@ -1,8 +1,8 @@
 class Jar2app < Formula
   desc "easily convert JAR files into Mac OS Applications"
   homepage "https://github.com/dante-biase/jar2app"
-  url "https://github.com/dante-biase/homebrew-x2x/releases/download/v1.0/jar2app"
-  sha256 "46355421c617af0bf596b60a543dd59be99981f77fe6694cb14fe3c8286a4558"
+  url "https://github.com/dante-biase/homebrew-x2x/releases/download/v1.0.2/jar2app"
+  sha256 "0f157e9783474dbfaa514d445b253a2aa752979fc562e65b66c7571fbd17822f"
 
   bottle :unneeded
 

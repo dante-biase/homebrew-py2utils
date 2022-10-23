3faea7ca08da75a332ce041a08d6b470bb130ca0
dante-biase <45440995+dante-biase@users.noreply.github.com>
2022-10-23 10:20:07 -0400

    Update py2app.rb

--- py2app.rb
+++ py2app.rb
@@ -4,8 +4,6 @@ class Py2app < Formula
   url "https://github.com/dante-biase/homebrew-x2x/releases/download/v1.0.1/py2app"
   sha256 "4cdfede5b390985efa12cf62d5defd29e3efeff35ca750267f3eb067220524ec"
 
-  bottle :unneeded
-
   def install
     bin.install "py2app"
   end

b70f85c54fd2162b7d5a808f2fbb9bfa77fd5837
dante-biase <45440995+dante-biase@users.noreply.github.com>
2022-10-23 10:19:07 -0400

    Update jar2app.rb

--- jar2app.rb
+++ jar2app.rb
@@ -4,8 +4,6 @@ class Jar2app < Formula
   url "https://github.com/dante-biase/homebrew-x2x/releases/download/v1.0.2/jar2app"
   sha256 "0f157e9783474dbfaa514d445b253a2aa752979fc562e65b66c7571fbd17822f"
 
-  bottle :unneeded
-
   def install
     bin.install "jar2app"
   end
